9eac43cfd696b3305dc14a16b22a8356c2c2c52c
Austin Erlandson <aerlandson@aspirehealthcare.com>
2020-03-20 12:27:52 -0500

    Dirp

--- public/bs-material-ui/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm src/*.bs.js
-mv ./bsconfig.json ./bsc.json; mv ./bsconfig-npm.json ./bsconfig.json
-cp ./../../README.md ./README.md
-yarn publish
-mv ./bsconfig.json ./bsconfig-npm.json; mv ./bsc.json ./bsconfig.json
-rm ./README.md
